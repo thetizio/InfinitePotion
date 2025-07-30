ac313e8f9591f779b0f128bdc03f89b12d7fc959
thetizio <matteobustischool@gmail.com>
2025-07-30 17:03:15 +0200

    probably 1st release

--- src/main/java/com/tizio/infinitepotion/InfinitePotion.java
+++ src/main/java/com/tizio/infinitepotion/InfinitePotion.java
@@ -1,32 +1,11 @@
 package com.tizio.infinitepotion;
 
-import net.minecraft.world.item.*;
+
 import org.slf4j.Logger;
-
 import com.mojang.logging.LogUtils;
-
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredItem;
-import net.neoforged.neoforge.registries.DeferredRegister;
-import org.spongepowered.asm.mixin.Mixin;
 
 @Mod(InfinitePotion.MODID)
 public class InfinitePotion {
@@ -36,7 +15,7 @@ public class InfinitePotion {
 
     public InfinitePotion(IEventBus modEventBus, ModContainer modContainer) {
 
-        LOGGER.info("Infinite Potion is starting");
+        LOGGER.info("Infinite Potion is loading");
 
     }
 
